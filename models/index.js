56f6611de781c80b4fcf396066907326eaa71ff7
Alejandro Martinez <cloudof.alemar@gmail.com>
2024-07-11 15:05:14 -0700

    1). Created the relationships/associations with the Models.

--- models/index.js
+++ models/index.js
@@ -1,4 +1,32 @@
 const TechUser = require( "./TechUser" );
 const Post = require( "./Post" );
+const Comment = require( "./Comment" );
 
-module.exports = { TechUser, Post };
+TechUser.hasMany( Post, {
+  foreignKey : "tech_user_id",
+  onDelete : "CASCADE"
+} );
+
+Post.belongsTo( Post, {
+  foreignKey : "tech_user_id"
+} );
+
+TechUser.hasMany( Comment, {
+  foreignKey : "tech_user_id",
+  onDelete : "CASCADE"
+} );
+
+Comment.belongsTo( TechUser, {
+  foreignKey : "tech_user_id"
+} );
+
+Post.hasMany( Comment, {
+  foreignKey : "post_id",
+  onDelete : "CASCADE"
+} );
+
+Comment.belongsTo( Post, {
+  foreignKey : "post_id"
+} );
+
+module.exports = { TechUser, Post, Comment };
