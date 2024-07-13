9fc1852e45b64c60a18d6fb6cb1e43b6bc8b3e9d
Alejandro Martinez <cloudof.alemar@gmail.com>
2024-07-13 16:05:34 -0700

    1). Added express-session and connect-session-sequelize and configured the sess variable in server.js

--- server.js
+++ server.js
@@ -4,17 +4,34 @@
 const path = require( "path" );
 
 const express = require( "express" );
-const sequelize = require( "./config/connection" );
 const exphbs = require( "express-handlebars" );
+const sequelize = require( "./config/connection" );
+const session = require( "express-session" );
+const SequelizeStore = require( "connect-session-sequelize" )( session.Store );
 
-const { TechUser, Post, Comment } = require( "./models" );
 const routes = require( "./controllers" );
 const helpers = require( "./utils/helpers" );
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 const hbs = exphbs.create( { helpers } );
+
+const sess = {
+  secret : "Super secret secret",
+  cookie : {
+    maxAge : 300000,
+    httpOnly : true,
+    secure : false,
+    sameSite : "strict"
+  },
+  resave : false,
+  saveUninitialized : true,
+  store : new SequelizeStore( {
+    db : sequelize
+  } )
+}
+
+app.use( session( sess ) );
 
 /*
   Configure Express.js to use handlebars as a templating engine
